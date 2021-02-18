ea391c48c5c1546fd9a8affacf3677afbf61895d
m-popa <darkdev@gmail.com>
2021-02-18 22:15:59 +0200

    pule, maturi2

--- app/Http/Controllers/Admin/CategoryCrudController.php
+++ app/Http/Controllers/Admin/CategoryCrudController.php
@@ -100,7 +100,7 @@ class CategoryCrudController extends CrudController
             ->label('Ascunde descrierea pentru categoriile cu poze inalte')
             ->hint('pentru torturi nunta?')
             ->type('select_from_array')
-            ->default('Vezi Produsul')
+            ->default('0')
             ->options(['0' => 'NU', '1' => 'DA'])
             ->size(6);
 
