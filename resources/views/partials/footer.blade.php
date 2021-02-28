ebddd671d65ac742c671b6770c50ae75b02bd03e
m-popa <darkdev@gmail.com>
2021-02-28 21:24:25 +0200

    moved menu to footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -45,18 +45,21 @@
 					<a class="h6 text-white mr-3 text-capitalize" href="{{ $category->url }}">{{ $category->name }}</a> 
 					@endforeach
 				</h3>
+				
 				<h3>
-
 					Social Media | 
 					<br class="d-block d-md-none"> 
 					<a class="h6 text-white mr-3" href="https://www.facebook.com/cofetariavilceanu" target="_blank">Facebook</a>
 					<a href="https://api.whatsapp.com/send?phone=+40769098648" class="h6 text-white mr-3">Whatsapp</a>
 				</h3>
-				<a href="https://anpc.ro/" class="h6 text-white mr-3" target="_blank">ANPC</a>
-				<a href="/alergeni" class="h6 text-white mr-3" target="_blank">Alergeni</a>
 			</div>
 			<div class="drepturi col-12 mt-4 pt-4">		
-				<a class="h6 text-white float-none float-sm-right" href="{{ route('contact') }}">Contactează-ne</a>
+				<a href="{{ route('contact') }}" 
+				class="h6 text-white float-none mx-2 float-sm-right">Contactează-ne</a>
+				<a href="https://anpc.ro/" 
+				class="h6 text-white float-none mx-2 float-sm-right" target="_blank">ANPC</a>
+				<a href="alergeni" 
+				class="h6 text-white float-none mx-2 float-sm-right">Alergeni</a>
 				<br class="d-block d-sm-none">
 				<span class="h6 text-white">© {{ now()->year }} Cofetaria Vilceanu. Toate drepturile rezervate.</span>
 			</div>
